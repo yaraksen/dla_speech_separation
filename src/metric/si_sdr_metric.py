406fcc7747e8ef39fb9aad4f0e145a3a5a5a07d8
Yaroslav Aksenov <yaroslav.o.aksenov@gmail.com>
2023-11-09 13:09:41 +0000

    adg

--- src/metric/si_sdr_metric.py
+++ src/metric/si_sdr_metric.py
@@ -10,6 +10,10 @@ class SiSDRMetric(BaseMetric, torch.nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.si_sdr = ScaleInvariantSignalDistortionRatio()
+    
+    def to(self, device):
+        self.si_sdr = self.si_sdr.to(device)
+        return self
 
     def __call__(self, pred_short: Tensor, target: Tensor, **kwargs):
         return self.si_sdr(pred_short, target)
