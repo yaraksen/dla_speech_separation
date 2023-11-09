bcfd0c425670b4eec4cfec854e51a8bbdad13d1a
Yaroslav Aksenov <yaroslav.o.aksenov@gmail.com>
2023-11-09 13:22:27 +0000

    asg

--- src/metric/si_sdr_metric.py
+++ src/metric/si_sdr_metric.py
@@ -1,10 +1,17 @@
 from typing import List
 
 import torch
-from torch import Tensor
+from torch import Tensor, zeros_like
+from torch.nn.functional import pad
 
 from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
 from src.base.base_metric import BaseMetric
+
+def to_real_length(t: Tensor, mixed_lens: Tensor) -> Tensor:
+    masked = zeros_like(t)
+    for row, len in enumerate(mixed_lens):
+        masked[row, :len] = t[row, :len]
+    return masked
 
 class SiSDRMetric(BaseMetric):
     def __init__(self, *args, **kwargs):
@@ -15,5 +22,12 @@ class SiSDRMetric(BaseMetric):
         self.si_sdr = self.si_sdr.to(device)
         return self
 
-    def __call__(self, pred_short: Tensor, target: Tensor, **kwargs):
+    def __call__(self, pred_short: Tensor, target: Tensor, mixed_lens, **kwargs):
+        target = to_real_length(target, mixed_lens)
+        pred_short = to_real_length(pred_short, mixed_lens)
+
+        pad_value = pred_short.shape[-1] - target.shape[-1]
+        assert pad_value >= 0, "pad value cannot be less 0"
+        target = pad(target, (0, pad_value))
+
         return self.si_sdr(pred_short, target)
