e7642a9652fb21c5f271e10f2aabf86b7c6c2337
Yaroslav Aksenov <yaroslav.o.aksenov@gmail.com>
2023-11-12 13:05:15 +0000

    asdg

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,7 +2,6 @@ from src.datasets.custom_audio_dataset import CustomAudioDataset
 from src.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from src.datasets.librispeech_dataset import LibrispeechDataset
 from src.datasets.ljspeech_dataset import LJspeechDataset
-from src.datasets.common_voice import CommonVoiceDataset
 from src.datasets.ss_dataset import SpeechSeparationDataset
 
 __all__ = [
@@ -10,6 +9,5 @@ __all__ = [
     "CustomDirAudioDataset",
     "CustomAudioDataset",
     "LJspeechDataset",
-    "CommonVoiceDataset",
     "SpeechSeparationDataset"
 ]
